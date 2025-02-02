6856f85ff64e20660e0fff08989fab87f7d972b3
0xBingo <therealbingops@gmail.com>
2025-02-02 03:26:27 +0100

    ci(tflint): add and fix tflint rules

--- main.tf
+++ main.tf
@@ -3,16 +3,17 @@
 # Initial Provider Configuration for Proxmox
 
 terraform {
+  required_version = ">= 1.10.0"
   required_providers {
     proxmox = {
-      source = "bpg/proxmox"
+      source  = "bpg/proxmox"
       version = ">= 0.70.0"
     }
   }
 }
 
 provider "proxmox" {
-  endpoint = var.proxmox_api_url
+  endpoint  = var.proxmox_api_url
   api_token = var.proxmox_api_token
-  insecure = true
+  insecure  = true
 }
